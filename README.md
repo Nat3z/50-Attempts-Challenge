9b1375af17a26123a3c67bab915f574f040509ba
Nat3z <you@example.com>
2021-11-26 19:33:08 -0800

    v2 update

---
+++ README.md
@@ -0,0 +1,11 @@
+# 50 Attempts Challenge
+Website and Javascript by [NatiaDev](https://github.com/Nat3z), original challenge idea by [Eric Van Wilderman](https://www.youtube.com/c/EricVanWilderman)
+
+# What is the 50 Attempts Challenge?
+To make this short, you have 50 attempts to beat as many recent levels as possible.
+
+# What does this website do?
+This website makes this process of getting recent levels easier by grabbing the most recent levels thru the [GD Browser API](https://gdbrowser.com/) and displaying it in a readable fasion. Not only that, it also displays the amount of remaining attempts you have left and will do the appropriate calculations when you submit a completed level.
+
+# Contributions/Software Used
+- [GD Browser API - GD Colon](https://gdbrowser.com/)
--- execute.js
+++ execute.js
@@ -1,24 +1,64 @@
 let rouletteList = [];
 
-let currentpage = 0;
-
-function addNewLevels() {
-    currentpage++;
-    fetch("https://gdbrowser.com/api/search/*?type=recent&page=" + currentpage).then(res => res.json()).then(data => {
-        data.forEach(level => {
-            challenges.push(level);
-        });
-    });
-}
-
+let currentpage = 1;
 fetch("https://gdbrowser.com/api/search/*?type=recent").then(res => res.json()).then(data => {
+    console.log("Added page 1");
     data.forEach(level => {
         rouletteList.push(level);
     });
 });
 
+setTimeout(() => {
+    console.log("Added page 2");
+    fetch("https://gdbrowser.com/api/search/*?type=recent&page=2").then(res => res.json()).then(data => {
+        data.forEach(level => {
+            rouletteList.push(level);
+        });
+    });
+}, 600);
+
+setTimeout(() => {
+    console.log("Added page 3");
+    fetch("https://gdbrowser.com/api/search/*?type=recent&page=3").then(res => res.json()).then(data => {
+        data.forEach(level => {
+            rouletteList.push(level);
+        });
+    });
+}, 400);
+
+setTimeout(() => {
+    console.log("Added page 4");
+    fetch("https://gdbrowser.com/api/search/*?type=recent&page=4").then(res => res.json()).then(data => {
+        data.forEach(level => {
+            rouletteList.push(level);
+        });
+    });
+}, 1200);
+
+setTimeout(() => {
+    console.log("Added page 5");
+    fetch("https://gdbrowser.com/api/search/*?type=recent&page=5").then(res => res.json()).then(data => {
+        data.forEach(level => {
+            rouletteList.push(level);
+        });
+    });
+}, 1600);
+
+setTimeout(() => {
+    console.log("Added page 6");
+    fetch("https://gdbrowser.com/api/search/*?type=recent&page=6").then(res => res.json()).then(data => {
+        data.forEach(level => {
+            rouletteList.push(level);
+        });
+    });
+}, 2300);
+
 let challenges = [];
 let ended = false;
+
+function showHelp() {
+    window.location = "https://github.com/Nat3z/50-Attempts-Challenge/blob/master/README.md"
+}
 
 function start() {
     for (let i = 0; i < rouletteList.length; i++) {
@@ -50,30 +90,38 @@ let topPX = 100;
 let skips = 0;
 let attempts = 50;
 
-function _submit(_id, _id_input, _challengeID, _id_skip) {
+function _submit(_id, _id_input, _challengeID, _id_skip, _id_coins) {
     try {
         const submit = document.getElementById("" + _id);
         const input = document.getElementById("" + _id_input);
         const challenge = document.getElementById("" + _challengeID);
         const skip = document.getElementById("" + _id_skip);
+        const coins = document.getElementById("" + _id_coins);
 
         let val = parseInt(input.value);
         if (!val) val = 1;
+        let coinsCollected = parseInt(coins.value);
+        if (!coinsCollected || coinsCollected > 3) coinsCollected = 0;
+
+        attempts += coinsCollected;
 
         if (challenges[0] == undefined) {
-            addNewLevels();
+            endGame();
+            document.getElementsByTagName("header")[0].getElementsByTagName("h1")[0].textContent = `${attempts} Attempts`;
+            document.getElementsByTagName("header")[0].getElementsByTagName("h1")[0].style.color = "green";
+            return;
         }
-
         submit.style.cssText = "display: none";
         input.style.cssText = "display: none";
         skip.style.cssText = "display: none";
+        coins.style.cssText = "display: none";
 
         attempts -= val;
         attempts++;
 
         document.getElementsByTagName("header")[0].getElementsByTagName("h1")[0].textContent = `${attempts} Attempts`;
         document.getElementsByTagName("header")[0].getElementsByTagName("h1")[0].style.left = "42%";
-        
+
         if (attempts <= 0) {
             attempts = 0;
             document.getElementsByTagName("header")[0].getElementsByTagName("h1")[0].textContent = `${attempts} Attempts`;
@@ -89,6 +137,7 @@ function _submit(_id, _id_input, _challengeID, _id_skip) {
         addChallenge();
     } catch (error) {
         alert(error)
+        console.log(error)
     }
 }
 
@@ -98,7 +147,7 @@ function skip(id) {
     document.querySelector(`#challenge-${id}`).getElementsByTagName("button")[0].style.display = "none";
     document.querySelector(`#challenge-${id}`).getElementsByTagName("button")[1].style.display = "none";
     attempts -= 4;
-
+    skips++;
     challenge.innerHTML += `
         <h3>Skipped</h3>
     `
@@ -114,6 +163,8 @@ function endGame() {
     topPX += 60;
 
     document.querySelector(`#challenge-${id}`).getElementsByTagName("input")[0].style.display = "none";
+    document.querySelector(`#challenge-${id}`).getElementsByTagName("input")[1].style.display = "none";
+
     document.querySelector(`#challenge-${id}`).getElementsByTagName("button")[0].style.display = "none";
     document.querySelector(`#challenge-${id}`).getElementsByTagName("button")[1].style.display = "none";
 
@@ -125,6 +176,7 @@ function endGame() {
         <div class="gameEnd" id="end">
             <h1>Game Ended!</h1>
             <h2>Levels Completed: ${id}</h2>
+            <h2>Skipped ${skips} Times.</h2>
             <h2>Refresh the page to play again.</h2>
         </div>
     `
@@ -145,7 +197,7 @@ function endGame() {
 
 function addChallenge() {
     var num = $s().random(challenges.length - 1);
-    const challenge = challenges[num];
+    let challenge = challenges[num];
 
     challenges.splice(num, 1);
     id += 1;
@@ -155,9 +207,10 @@ function addChallenge() {
         <div class="challenge" id="challenge-${id}">
             <h1>${challenge["name"]}</h1>
             <h2>${challenge["id"]}</h2>
+            <input type="number" id="coins-${id}" class="coins" placeholder="Coins Collected">
 
-            <input type="number" id="input-${id}">
-            <button id="submit-${id}" onclick='_submit("submit-${id}", "input-${id}", "challenge-${id}", "skip-${id}")'>Submit</button>
+            <input type="number" id="input-${id}" placeholder="Total Attempts">
+            <button id="submit-${id}" onclick='_submit("submit-${id}", "input-${id}", "challenge-${id}", "skip-${id}", "coins-${id}")'>Submit</button>
             <button class="skip" id="skip-${id}" onclick='skip("${id}")'>Skip</button>
         </div>
     `
@@ -174,4 +227,14 @@ function addChallenge() {
     setTimeout(() => {
         document.getElementById(`challenge-${id}`).style.top = `${topPX}px`
     }, 1000);
+}
+
+function addNewLevels() {
+    currentpage++;
+    console.log("Added New Challenges");
+    fetch("https://gdbrowser.com/api/search/*?type=recent&page=2").then(res => res.json()).then(data => {
+        data.forEach(level => {
+            challenges.push(level);
+        });
+    });
 }
